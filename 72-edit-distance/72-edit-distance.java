f0ef7e99699b777fc99950e3dcca50e8c533f6cb
Varun Jadon <91825162+ThakurSahab0@users.noreply.github.com>
2022-09-21 10:34:17 +0530

    Time: 8 ms (69.36%), Space: 45.4 MB (13.42%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,27 +1,23 @@
 public class Solution {
-    public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        
-        int[][] cost = new int[m + 1][n + 1];
-        for(int i = 0; i <= m; i++)
-            cost[i][0] = i;
-        for(int i = 1; i <= n; i++)
-            cost[0][i] = i;
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(word1.charAt(i) == word2.charAt(j))
-                    cost[i + 1][j + 1] = cost[i][j];
-                else {
-                    int a = cost[i][j];
-                    int b = cost[i][j + 1];
-                    int c = cost[i + 1][j];
-                    cost[i + 1][j + 1] = a < b ? (a < c ? a : c) : (b < c ? b : c);
-                    cost[i + 1][j + 1]++;
-                }
-            }
-        }
-        return cost[m][n];
-    }
+public int minDistance(String word1, String word2) {
+char[] w1 = word1.toCharArray(); char[] w2 = word2.toCharArray();
+int m = word1.length(); int n= word2.length();
+if(m==0) return n; if(n==0) return m;
+int[][] dp = new int[m][n]; for(int i=0;i<m;i++) Arrays.fill(dp[i],-1);
+return f(m-1,n-1,w1,w2,dp);
+}
+public static int f(int i1, int i2, char[] w1, char[] w2,int[][] dp){
+if(i1==-1 && i2==-1) return 0;
+if(i1==-1) return i2+1; //implicitly i2!=-1 returns num of remaining chars in w2, insert em in w1
+if(i2==-1) return i1+1; //implicitly i1!=-1 returns rem chars in w1,delete em in w1
+if(dp[i1][i2]!=-1) return dp[i1][i2];
+int same,insert,delete,replace; same=insert=delete=replace= (int)1e9;
+if(w1[i1]==w2[i2]) same = f(i1-1, i2-1, w1, w2,dp);
+else{
+insert = 1 + f(i1, i2-1, w1, w2,dp);
+delete = 1 + f(i1-1, i2, w1,w2,dp);
+replace = 1 + f(i1-1,i2-1,w1,w2,dp);
+}
+return dp[i1][i2] = Math.min(Math.min(insert,delete), Math.min(replace,same));
+}
 }
